2cc67ff60b22c7611cecd602a559d84384a8b031
Peter <peter@peterphi.com>
2013-11-06 16:50:09 +0000

    Remove index service implementation... as a service registry for development use it's OK but as a service registry for dynamic services it's not great (circular dependencies, for example, cannot be resolved whilst ensuring services don't start without their required services existing). This code could be added back in at a later stage with these problems fixed (and at that time it should hopefully also be possible to simplify the client-side code...).
    An alternative to re-adding this would be to simply use another service registry implementation...

--- guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/setup/AbstractRESTGuiceSetup.java
+++ guice/webapp/src/main/java/com/peterphi/std/guice/web/rest/setup/AbstractRESTGuiceSetup.java
@@ -5,7 +5,6 @@ import com.google.inject.Module;
 import com.peterphi.std.guice.apploader.GuiceSetup;
 import com.peterphi.std.guice.web.rest.CoreRestServicesModule;
 import com.peterphi.std.guice.web.rest.scoping.ServletScopingModule;
-import com.peterphi.std.indexservice.rest.client.guice.IndexServiceModule;
 import com.peterphi.std.io.PropertyFile;
 import org.apache.log4j.Logger;
 
@@ -17,13 +16,6 @@ import java.util.List;
 public abstract class AbstractRESTGuiceSetup implements GuiceSetup
 {
 	private static final Logger log = Logger.getLogger(AbstractRESTGuiceSetup.class);
-
-	public static final String INDEX_SERVICE_ENDPOINT = "service.IndexRestService.endpoint";
-
-	/**
-	 * The override for the index service enable property - allows services to opt out of a globally configured index service
-	 */
-	public static final String DISABLE_INDEX_SERVICE = "framework.indexservice.disabled";
 
 	/**
 	 * The override for the core REST services
@@ -42,21 +34,6 @@ public abstract class AbstractRESTGuiceSetup implements GuiceSetup
 		}
 		else {
 			log.info("REST Core Services disabled by config parameter");
-		}
-
-		// Enable the index service if the webapp wants to use it
-		final boolean indexServiceDisabled = config.getBoolean(DISABLE_INDEX_SERVICE, false);
-
-		final boolean hasIndexEndpoint = (config.get(INDEX_SERVICE_ENDPOINT, null) != null);
-
-		if (hasIndexEndpoint && !indexServiceDisabled)
-		{
-			log.info("Enabling index service capabilities...");
-			modules.add(new IndexServiceModule());
-		}
-		else
-		{
-			log.info("Index service capabilities were not enabled");
 		}
 
 		addModules(modules, config);
